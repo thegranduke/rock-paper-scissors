6ad1d508cd6331102228f26fb4badface4c166ed
Eino <einompi@gmail.com>
2024-04-20 03:19:27 +0200

    Create playGame function and put all the functions together to get a playable game

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // array to store all possible choices //
 let possibleChoices = ["rock","paper","scissor"];
+const NUMBER_OF_ROUNDS = 5;
 
 
 function getComputerChoice(){
@@ -35,14 +36,42 @@ function playRound(getComputerChoice,getPlayerChoice){
               playerChoice == "scissor" && computerChoice == "paper" 
             ){
         
-        verdict = `You win! ${playerChoice} beats ${computerChoice}`
+        verdict = `You win! ${playerChoice} beats ${computerChoice}`;
 
     }
     else {
-        verdict = `You lose! ${computerChoice} beats ${playerChoice}`
+        verdict = `You lose! ${computerChoice} beats ${playerChoice}`;
     }
 
     return verdict
 }
 
-console.log(playRound(getComputerChoice,getPlayerChoice));
+function playGame(playRound,NUMBER_OF_ROUNDS){
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < NUMBER_OF_ROUNDS; i++){
+        let verdict = playRound(getComputerChoice,getPlayerChoice);
+        console.log(verdict);
+        verdict = verdict.charAt(4);
+        if (verdict == "w"){
+            playerScore = playerScore + 1;
+        }
+        else if (verdict == "l"){
+            computerScore = computerScore + 1;
+        }
+    
+    }
+
+    if (playerScore == computerScore){
+        return "Its a draw!"
+    }
+    else if (playerScore > computerScore){
+        return "You win!"    
+    }  
+    else{
+        return "Computer wins!"
+    }
+}
+
+console.log(playGame(playRound,NUMBER_OF_ROUNDS));
