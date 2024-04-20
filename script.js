cd8bbe3c13a66e3650be9522d928cc3c91406fed
Eino <einompi@gmail.com>
2024-04-20 02:47:49 +0200

    Create playRound function and update the getPlayerChoice function to handle invalid input

--- script.js
+++ script.js
@@ -1,21 +1,48 @@
+// array to store all possible choices //
 let possibleChoices = ["rock","paper","scissor"];
 
-function getComputerChoice(possibleChoices){
+
+function getComputerChoice(){
     let randomIndex = Math.floor(Math.random() * possibleChoices.length);
     let computerChoice = possibleChoices[randomIndex];
     return computerChoice;
 }
 
+
 function getPlayerChoice(){
-    let userChoice = prompt("Enter rock, paper or scissors");
-    userChoice = userChoice.toLowerCase();
-    userChoice = userChoice.trim();
-    if (possibleChoices.includes(userChoice)){
-        return userChoice;
-    }
-    else{
-        console.log("Invalid value entered")
-    }
+
+    let playerChoice = null;
+    do{
+        playerChoice = prompt("Enter rock, paper or scissor");
+        playerChoice = playerChoice.toLowerCase();
+        playerChoice = playerChoice.trim();
+    } while (!(possibleChoices.includes(playerChoice)))
+
+    return playerChoice;
 }
 
-console.log(getPlayerChoice())
+
+function playRound(getComputerChoice,getPlayerChoice){
+    let verdict = null;
+    let playerChoice = getPlayerChoice()
+    let computerChoice = getComputerChoice()
+
+    if (computerChoice == playerChoice){
+        verdict = "It's a draw!"
+    }
+    else if ( playerChoice == "rock" && computerChoice== "scissor" ||
+              playerChoice == "paper" && computerChoice == "rock" ||
+              playerChoice == "scissor" && computerChoice == "paper" 
+            ){
+        
+        verdict = `You win! ${playerChoice} beats ${computerChoice}`
+
+    }
+    else {
+        verdict = `You lose! ${computerChoice} beats ${playerChoice}`
+    }
+
+    return verdict
+}
+
+console.log(playRound(getComputerChoice,getPlayerChoice));
